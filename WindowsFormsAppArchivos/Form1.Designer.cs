2dc537d18889806e57d3eccd798880684d52b307
victor.gabardini <victor.gabardini@AR-IT18789>
2021-06-17 14:13:37 -0300

    Add project files.

---
+++ WindowsFormsAppArchivos/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WindowsFormsAppArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Explorar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Explorar
+            // 
+            this.Explorar.Location = new System.Drawing.Point(60, 834);
+            this.Explorar.Name = "Explorar";
+            this.Explorar.Size = new System.Drawing.Size(174, 39);
+            this.Explorar.TabIndex = 1;
+            this.Explorar.Text = "Explorar";
+            this.Explorar.UseVisualStyleBackColor = true;
+            this.Explorar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1254, 789);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 901);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Explorar);
+            this.Name = "Form1";
+            this.Text = "Manejo de Archivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Explorar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsAppArchivos/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppArchivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            string path = fbd.SelectedPath;
+            if (path != "")
+                textBox1.Text = GetFiles(path);
+            else
+                MessageBox.Show("No seleccionó ningún directorio");
+        }
+
+        public static string GetFiles(string path)
+        {
+            string str = "";
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+//            FileInfo[] archivos = dir.GetFiles("*.*", SearchOption.AllDirectories);
+            foreach (FileSystemInfo f in dir.GetFileSystemInfos())
+            {
+                if(f is FileInfo)
+                    {
+                        FileInfo j = f as FileInfo;
+                        str += "" + f.Name + " - " + j.Length + "\r\n";
+                }
+                else
+                {
+                    str += "\\" + f.Name + "\r\n";
+                }
+
+            }
+            return str;
+        }
+    }
+}
